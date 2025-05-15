0313d450b84987933e7e84c6ab22d3477283f6d0
Marcelo <marcelocardoso020304@gmail.com>
2025-05-14 22:21:54 -0300

    Iniciado o streamlit

---
+++ analise/analiseStreamlit.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="Dashboard de Despesas", page_icon='🏎️', layout='wide')
+
+df = pd.read_excel("Despesas_Tratadas.xlsx")
+
+st.sidebar.header("Selecione os Filtros")
+
+
+tipos = st.sidebar.multiselect(
+    "Tipos",
+    options=df["tipo"].unique(),
+    default=df["tipo"].unique(),
+    key="tipo"
+)
+
+setores = st.sidebar.multiselect(
+    "Setores",
+    options=df["setor"].unique(),
+    default=df["setor"].unique(),
+    key="setor"
+)
+
+
+df_selection = df.query("tipo in @tipos and setor in @setores")
+
+def Home():
+    total_despesas = df["valor"].sum()
+    media = df["valor"].mean()
+    mediana = df["valor"].median()
+        
+    total1, total2, total3 = st.columns(3)
+    with total1:
+            # Apresentar indicadores rápidos
+      st.metric("Total de Despesas", value=int(total_despesas))
+    with total2:
+            st.metric("Média por Produto", value=f"{media:.2f}")
+    with total3:
+            st.metric("Mediana", value=int(mediana))
+            
+    st.markdown("---")
+    
+    df_selection['mes_ano'] = df_selection['data'].dt.to_period('Y').astype(str)
+    df_grouped = (
+        df_selection
+        .groupby(['mes_ano', 'setor'])['valor']
+        .sum()
+        .reset_index())
+    
+    fig_barras = px.bar(
+        df_grouped , 
+        x='mes_ano',
+        y='valor',
+        color='setor',
+        barmode='group',
+        title="Quantidade de despesa por setor"
+    )
+    
+    st.plotly_chart(fig_barras, use_container_width=True)
+    
+def sideBar():
+    with st.sidebar:
+        selecionado = option_menu(
+            menu_title="Menu",
+            options=["Home", "Gráficos"],
+            icons=["house", "bar-chart"],
+            default_index=0
+        )
+    if selecionado == "Home":
+        Home()
+
+sideBar()
+    
--- scrappingTreino.py
+++ scrappingTreino.py
@@ -38,7 +38,6 @@ driver.get(url_base)
 time.sleep(5) 
 
 dic_despesas = {
-    "id_despesas":[], 
     "data":[],
     "tipo":[],
     "setor":[],
@@ -65,55 +64,71 @@ while True:
     
     for despesa in despesas:
         try:
-            id_despesa = despesa.find_element(By.CLASS_NAME, "td_id_despesa").text
-            data = despesa.find_element(By.CLASS_NAME, "td_data").text
-            tipo = despesa.find_element(By.CLASS_NAME, "td_tipo").text
-            setor = despesa.find_element(By.CLASS_NAME, "td_setor").text
-            valor = despesa.find_element(By.CLASS_NAME, "td_valor").text
-            fornecedor = despesa.find_element(By.CLASS_NAME, "td_fornecedor").text
+            data = despesa.find_element(By.CLASS_NAME, "td_data").text.strip()
+            tipo = despesa.find_element(By.CLASS_NAME, "td_tipo").text.strip()
+            setor = despesa.find_element(By.CLASS_NAME, "td_setor").text.strip()
+            valor = despesa.find_element(By.CLASS_NAME, "td_valor").text.strip()
+            fornecedor = despesa.find_element(By.CLASS_NAME, "td_fornecedor").text.strip()
             
-            
-            dic_despesas["id_despesas"].append(id_despesa)
+                 
             dic_despesas["data"].append(data)
             dic_despesas["tipo"].append(tipo)
             dic_despesas["setor"].append(setor )
             dic_despesas["valor"].append(valor)
             dic_despesas["fornecedor"].append(fornecedor)
            
-            print(f"{id_despesa} - {data} - {tipo} - {setor} - {valor} - {fornecedor}")
+            print(f"{data} - {tipo} - {setor} - {valor} - {fornecedor}")
         except Exception as e:
             print("Erro ao coletar dados", e)
             
-        try:
-            botao_orcamento = WebDriverWait(driver, 10).until(
-                ec.element_to_be_clickable((By.XPATH, "//button[contains(text())='Orçamentos']"))
-            )
-            if botao_orcamento:
-                driver.execute_script("arguments[0].click();", botao_orcamento)
-                time.sleep(5)
-        except Exception:
+    try:
+        botao_orcamento = WebDriverWait(driver, 10).until(
+                ec.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Orçamentos')]")))
+        if botao_orcamento:
+            driver.execute_script("arguments[0].click();", botao_orcamento)
+            print("clicadp")
+            time.sleep(5)
+    except Exception:
             print("Error")
             
         
-        try:
-            WebDriverWait(driver, 10).until(ec.presence_of_all_elements_located((By.XPATH, "//tr[contains(@style, \"border-bottom: 1px solid rgb(221, 221, 221)\")]")))
-        except TimeoutException:
-            print("Tempo de espera excedido!")
+    try:
+        WebDriverWait(driver, 10).until(ec.presence_of_all_elements_located((By.XPATH, "//tr[contains(@style, \"border-bottom: 1px solid rgb(221, 221, 221)\")]")))
+    except TimeoutException:
+        print("Tempo de espera excedido!")
         
     orcamentos = driver.find_elements(By.XPATH, "//tr[contains(@style, \"border-bottom: 1px solid rgb(221, 221, 221)\")]")
     
     for orcamento in orcamentos:
         try:
-            data = despesa.find_element(By.CLASS_NAME, "td_data").text
-            tipo = despesa.find_element(By.CLASS_NAME, "td_tipo").text
-            setor = despesa.find_element(By.CLASS_NAME, "td_setor").text
-            valor = despesa.find_element(By.CLASS_NAME, "td_valor").text
-            fornecedor = despesa.find_element(By.CLASS_NAME, "td_fornecedor").text
+            setor = orcamento.find_element(By.CLASS_NAME, "td_setor").text.strip()
+            mes = orcamento.find_element(By.CLASS_NAME, "td_mes").text.strip()
+            ano = orcamento.find_element(By.CLASS_NAME, "td_ano").text.strip()
+            valor_previsto = orcamento.find_element(By.CLASS_NAME, "td_valor_previsto").text.strip()
+            valor_realizado = orcamento.find_element(By.CLASS_NAME, "td_valor_realizado").text.strip()
+            
+            dic_orcamento["setor"].append(setor)
+            dic_orcamento["mes"].append(mes)
+            dic_orcamento["ano"].append(ano)
+            dic_orcamento["valor_previsto"].append(valor_previsto)
+            dic_orcamento["valor_realizado"].append(valor_realizado)
             
            
-            print(f"{id_despesa} - {data} - {tipo} - {setor} - {valor} - {fornecedor}")
+            print(f"{setor} - {mes} - {ano} - {valor_previsto} - {valor_realizado}")
         except Exception as e:
             print("Erro ao coletar dados", e)
+    break
+
+driver.quit()
+
+df_despesas = pd.DataFrame(dic_despesas)
+print(f"Arquivo 'Despesas' salvo com sucesso {len(df_despesas)} amazenados")
+df_despesas.to_excel("Despesas.xlsx", index=False)
+
+df_orcamento = pd.DataFrame(dic_orcamento)
+print(f"Arquivo 'orcamento' salvo com sucesso {len(df_orcamento)} amazenados")
+df_orcamento.to_excel("Orcamento.xlsx", index=False)
+
             
         
         
